5858a8b3ef2f00c655eb65c530f6e21f282ee3c3
Corey Rome <coreyrome2@gmail.com>
2018-01-30 08:32:44 -0600

    added stripe api, stock api, and new libraries

--- AfterMarketStocks/Models/AlphaStock.cs
+++ AfterMarketStocks/Models/AlphaStock.cs
@@ -2,12 +2,77 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Newtonsoft.Json;
+using System.Net;
+
 
 namespace AfterMarketStocks.Models
 {
-    public class AlphaStock
+   
+    public partial class Welcome
     {
+        [JsonProperty("Meta Data")]
+        public MetaData MetaData { get; set; }
 
-         
+        [JsonProperty("Time Series (1min)")]
+        public Dictionary<string, TimeSeries1Min> TimeSeries1Min { get; set; }
+    }
+
+    public partial class MetaData
+    {
+        [JsonProperty("1. Information")]
+        public string The1Information { get; set; }
+
+        [JsonProperty("2. Symbol")]
+        public string The2Symbol { get; set; }
+
+        [JsonProperty("3. Last Refreshed")]
+        public System.DateTime The3LastRefreshed { get; set; }
+
+        [JsonProperty("4. Interval")]
+        public string The4Interval { get; set; }
+
+        [JsonProperty("5. Output Size")]
+        public string The5OutputSize { get; set; }
+
+        [JsonProperty("6. Time Zone")]
+        public string The6TimeZone { get; set; }
+    }
+
+    public partial class TimeSeries1Min
+    {
+        [JsonProperty("1. open")]
+        public string The1Open { get; set; }
+
+        [JsonProperty("2. high")]
+        public string The2High { get; set; }
+
+        [JsonProperty("3. low")]
+        public string The3Low { get; set; }
+
+        [JsonProperty("4. close")]
+        public string The4Close { get; set; }
+
+        [JsonProperty("5. volume")]
+        public string The5Volume { get; set; }
+    }
+
+    public partial class Welcome
+    {
+        public static Welcome FromJson(string json) => JsonConvert.DeserializeObject<Welcome>(json, Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this Welcome self) => JsonConvert.SerializeObject(self, Converter.Settings);
+    }
+
+    public class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+        };
     }
 }
