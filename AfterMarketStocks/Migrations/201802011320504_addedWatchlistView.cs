9993c44b398f55d8723a096aec921f3aa491397b
Corey Rome <coreyrome2@gmail.com>
2018-02-01 07:24:20 -0600

    added watchlist add from mystocks

---
+++ AfterMarketStocks/Migrations/201802011320504_addedWatchlistView.cs
@@ -0,0 +1,16 @@
+namespace AfterMarketStocks.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addedWatchlistView : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
